e95a86cdc178beee4460f3a0021e8a4df7c4c6b4
Scott Moore <woodwindscott@yahoo.com>
2019-07-11 21:25:55 -0400

    Initial Files

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+console.log("Wins: " + wins);
+console.log("Losses: " + losses);
+console.log("Guesses Left: " + guessesLeft);
+console.log(letters.length);
