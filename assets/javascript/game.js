ecb2daf1558f56ea1bff5c4ee742adb7a4ee5d35
Scott Moore <woodwindscott@yahoo.com>
2019-07-16 16:44:24 -0400

    Updated Logic to run game properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,91 @@
+// Setup of initial variables
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
-
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var guessesLeft = 9;
+var computerGuess;
 
-console.log("Wins: " + wins);
-console.log("Losses: " + losses);
-console.log("Guesses Left: " + guessesLeft);
-console.log(letters.length);
+//Create functions to display scores, guesses remaining and guessed letters + computer Guess
+function displayWins() {
+    var targetWin = document.getElementById("win");
+    targetWin.textContent = wins;
+}
+
+function displayLosses() {
+    var targetLose = document.getElementById("lose");
+    targetLose.textContent = losses;
+}
+
+function displayGuessesLeft() {
+    var targetGuessLeft = document.getElementById("guessLeft");
+    targetGuessLeft.textContent = guessesLeft;
+}
+
+function resetGuesses() {
+    document.getElementById("guessed").innerHTML = "";
+}
+
+function newComputerGuess() {
+    computerGuess = letters[Math.floor(Math.random() * 26)];
+    console.log("New Computer Guess: " + computerGuess);
+}
+
+//Computer guess random letter from array
+newComputerGuess();
+
+//Event listener
+document.onkeyup = function(event) {
+    var userGuess = event.key.toLowerCase();
+    var userGuessCode = event.keyCode;
+
+    displayWins();
+    displayLosses();
+    
+    var targetGuessSoFar = document.getElementById("guessed");
+
+    //Error checking - make sure they select a letter
+    if (userGuessCode >= 65 && userGuessCode <= 90) {
+        guessesLeft--;
+        displayGuessesLeft();
+
+        //If guessed correctly...
+        if (userGuess === computerGuess && guessesLeft >= 0) {
+            wins++;
+            guessesLeft = 9;
+            newComputerGuess();
+            displayGuessesLeft();
+
+            var newSpan = document.createElement("span");
+            newSpan.textContent = userGuess + ", ";
+            targetGuessSoFar.appendChild(newSpan);
+
+            alert(userGuess + " is correct!");
+
+            resetGuesses();
+            displayWins();
+
+        //If guessed incorrectly and there are guesses remaining...
+        } else if (userGuess !== computerGuess && guessesLeft > 0) {
+          
+            var newSpan = document.createElement("span");
+            newSpan.textContent = userGuess + ", ";
+            targetGuessSoFar.appendChild(newSpan);
+
+        //No more guesses remaining.  You lose.
+        } else {
+            losses++;
+            guessesLeft = 9;
+
+            var newSpan = document.createElement("span");
+            newSpan.textContent = userGuess + ", ";
+            targetGuessSoFar.appendChild(newSpan);
+            
+            alert("The computer guessed " + computerGuess + ". Try again.")
+
+            newComputerGuess();
+            resetGuesses();
+            displayGuessesLeft();
+            displayLosses();
+        }
+    }
+}
